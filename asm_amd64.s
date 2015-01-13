3762208c115b54dad0f4e6297b3fd83324df3c45
philhofer <phofer@umich.edu>
2015-01-13 09:32:36 -0500

    for x86_64, release can be a simple MOVL

--- asm_amd64.s
+++ asm_amd64.s
@@ -3,25 +3,23 @@
 #define NOSPLIT 4
 
 TEXT ·Lock(SB),NOSPLIT,$0-8
-	MOVQ   l+0(FP), BP
+	MOVQ   l+0(FP), BP		// BP = l
 
 spin:
-	MOVL   $1, AX
-	LOCK          
-	XCHGL  AX, 0(BP)        
-	TESTL  AX, AX
-	JNZ    wait
-	RET
+	MOVL   $1, AX			// AX = 1      
+	XCHGL  AX, 0(BP)        // swap(AX, *BP)
+	TESTL  AX, AX			// if AX != 0
+	JNZ    wait				// goto wait
+	RET						// done
 
 wait:
-	PAUSE
-	JMP    spin
+	PAUSE					// pause
+	JMP    spin				// try again
 
 
 TEXT ·Unlock(SB),NOSPLIT,$0-8
-	MOVQ	l+0(FP), BP
-	MOVL    $0, AX
-	LOCK
-	XCHGL   AX, 0(BP)
-	RET
+	MOVQ 	l+0(FP), BP		// BP = l
+	XORL    AX, AX			// AX = 0
+	MOVL    AX, 0(BP)		// set *l=0
+	RET						// return
 
